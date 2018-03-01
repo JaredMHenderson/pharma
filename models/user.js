ad6d991ab5be52f8b3d0f54da9b3123771073940
cataire <mail.for.mu@gmail.com>
2018-02-28 21:13:35 -0700

    models

--- models/user.js
+++ models/user.js
@@ -1,16 +1,28 @@
+const moment = require('moment');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: {
       type: DataTypes.STRING,
       allowNull: false
     },
+
     DOB: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      set(val){
+        this.setDataValue('DOB', val);
+        this.setDataValue('age', moment().diff(moment(val)));
+      }
     },
     prescriptions: {
       type: DataTypes.STRING,
       allowNull: true
+    },
+
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
 
   });
