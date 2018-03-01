8100d44eb27b2218b6ff4427be8102a25bbaca80
cataire <mail.for.mu@gmail.com>
2018-02-28 21:29:49 -0700

    sync

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-
- app.listen(port, function() {
-   console.log("App listening on port " + port);
- });
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(port, function() {
+    console.log("App listening on port " + port);
+  });
+});
