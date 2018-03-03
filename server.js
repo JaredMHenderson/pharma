de7b373352586a75d12e36a347857692c2914240
Nick Moore <nickmoore04@gmail.com>
2018-03-03 12:08:11 -0700

    added pages

--- server.js
+++ server.js
@@ -8,18 +8,15 @@ const app = express();
 
 const exphbs = require("express-handlebars");
 
-app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
-
+app.use(express.static("public"));
 
  app.listen(port, function() {
    console.log("App listening on port " + port);
